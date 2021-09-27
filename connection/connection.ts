f36bce941f4a663c30c06cbe1dd150e6390f8e45
Rebeca Palmeira <rebeca.palmeira@gmail.com>
2021-09-27 06:43:15 -0300

    mudança de credencial Mongo

--- connection/connection.ts
+++ connection/connection.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 export = ()=>{ 
     mongoose.Promise = global.Promise;
 
-    mongoose.connect('mongodb+srv://rebeca@cluster0.9jadt.mongodb.net/bancoSoul?retryWrites=true&w=majority').then(()=>{
+    mongoose.connect('mongodb+srv://rebeca_palmeira:rebeca_palmeira@cluster0.9jadt.mongodb.net/bancoSoul?retryWrites=true&w=majority').then(()=>{
         console.log('Teu banco conectou, mané');
     }).catch((error: Error)=>{
         console.log(`Deu ruim, neguin: ${error}`);
